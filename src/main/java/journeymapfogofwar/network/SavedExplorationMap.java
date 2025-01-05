be429bb7e0cf704dd3386837fdc65d1ba018dade
Tucker Wood <twood@archetyp.com>
2025-01-05 12:32:59 -0800

    Adds Persistent Exploration Hashmap

---
+++ src/main/java/journeymapfogofwar/network/SavedExplorationMap.java
@@ -0,0 +1,74 @@
+package journeymapfogofwar.network;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.saveddata.SavedData;
+
+import static net.minecraftforge.network.NetworkDirection.PLAY_TO_CLIENT;
+import static net.minecraftforge.network.NetworkDirection.PLAY_TO_SERVER;
+
+import java.util.HashMap;
+import java.util.Set;
+
+import journeymap.client.api.display.PolygonOverlay;
+import journeymapfogofwar.JourneymapAdditions;
+import journeymapfogofwar.network.PacketRegistry;
+import journeymapfogofwar.network.packet.ChunkInfoPacket;
+
+public class SavedExplorationMap extends SavedData{
+        private HashMap<ChunkPos, String> exploredChunks; //ChunkPos, name of player who discovered it
+
+        public Boolean isExplored(ChunkPos pos) {
+            return exploredChunks.containsKey(pos);
+        }
+
+        public void exploreChunk(ChunkPos pos, ServerPlayer discoverer) {
+            exploredChunks.put(pos, discoverer.getScoreboardName());
+            setDirty();
+        }
+
+        public static SavedExplorationMap create() {
+            return new SavedExplorationMap();
+        }
+
+        public static SavedExplorationMap load(CompoundTag tag) {
+            SavedExplorationMap data = create();
+            //int testInt = tag.getInt("test");
+            //load all entries back from string representation as hashmap entries
+            Set<String> keys = tag.getAllKeys();
+            for (String key : keys) {
+                data.exploredChunks.put(chunkFromString(key), tag.getString(key));
+            }
+            //data.test = testInt;
+            return data;
+	    }
+
+        public CompoundTag save(CompoundTag tag) {
+            //tag.put("explorationMap", exploredChunks);
+            for (ChunkPos key : exploredChunks.keySet()) {
+                //tag.putValue(key, exploredChunks.get(key))
+                tag.putString(chunkToString(key), exploredChunks.get(key));
+            }
+            return tag;
+        }
+
+        public static void accessMap(MinecraftServer server) {
+            server.overworld().getDataStorage().computeIfAbsent(SavedExplorationMap::load, SavedExplorationMap::create, "explorationMap");
+        }
+        //CODE: add support for other dimensions based on serverplayer level dimension
+
+        private static String chunkToString(ChunkPos pos) {
+            return pos.x + "_" + pos.z;
+        }
+
+        private static ChunkPos chunkFromString(String posString) {
+            int posX = Integer.parseInt(posString.substring(0, posString.indexOf("_")));
+            int posZ = Integer.parseInt(posString.substring(posString.indexOf("_") + 1));
+            return new ChunkPos(posX, posZ);
+        }
+
+    
+}
--- src/main/java/journeymapfogofwar/network/dispatch/ServerNetworkDispatcher.java
+++ src/main/java/journeymapfogofwar/network/dispatch/ServerNetworkDispatcher.java
@@ -1,13 +1,21 @@
 package journeymapfogofwar.network.dispatch;
 
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.monitoring.jmx.MinecraftServerStatistics;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.loading.targets.FMLServerLaunchHandler;
+import net.minecraftforge.server.ServerLifecycleHooks;
 
 import static net.minecraftforge.network.NetworkDirection.PLAY_TO_CLIENT;
+import static net.minecraftforge.network.NetworkDirection.PLAY_TO_SERVER;
 
 import journeymapfogofwar.JourneymapAdditions;
 import journeymapfogofwar.network.PacketRegistry;
+import journeymapfogofwar.network.SavedExplorationMap;
 import journeymapfogofwar.network.packet.ChunkInfoPacket;
 
 public class ServerNetworkDispatcher
@@ -15,8 +23,20 @@ public class ServerNetworkDispatcher
     public static void sendChunkInfoPacket(ServerPlayer player, ChunkPos chunkPos)
     {
         //LevelChunk chunk = player.server.getLevel(null)
+        MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+        SavedExplorationMap.accessMap(server);
+        CompoundTag tag = new CompoundTag();
+        SavedExplorationMap map = SavedExplorationMap.load(tag);
         LevelChunk chunk = player.level().getChunk(chunkPos.x, chunkPos.z);
-        PacketRegistry.REGISTRY.sendTo(new ChunkInfoPacket(JourneymapAdditions.isSlimeChunk(chunk), chunkPos, true), player.connection.connection, PLAY_TO_CLIENT);
+        ChunkPos playerPos = player.chunkPosition();
+        //distance to check
+        Boolean isRevealed = map.isExplored(chunkPos);
+        if (!isRevealed) {
+            if (Math.sqrt(Math.pow(chunkPos.x - playerPos.x, 2) + Math.pow(chunkPos.z - playerPos.z, 2)) <= 6) {
+                map.exploreChunk(chunkPos, player);
+            }
+        }
+        PacketRegistry.REGISTRY.sendTo(new ChunkInfoPacket(JourneymapAdditions.isSlimeChunk(chunk), chunkPos, isRevealed), player.connection.connection, PLAY_TO_CLIENT);
     }
 
 }
